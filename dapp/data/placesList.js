02518024fe7079f9fb95da98a6838015a153fdbe
Gera Barboni <gerardobarboni@gmail.com>
2022-08-05 18:02:20 -0300

    error aspect-radio arreglado

---
+++ dapp/data/placesList.js
@@ -0,0 +1,47 @@
+const placesList = [
+  {
+    id: 'Barto1',
+    name: 'Barto',
+    ubi: 'Poeta Lugones 668',
+    imgPort:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    desc:
+      'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+  },
+  {
+    id: 'Wallas2',
+    name: 'Wallas',
+    ubi: 'Poeta Lugones 668',
+    imgPort:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    desc:
+      'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+  },
+  {
+    id: 'Porto3',
+    name: 'Porto',
+    ubi: 'Poeta Lugones 668',
+    imgPort:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    desc:
+      'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+  },
+  {
+    id: 'Morocha4',
+    name: 'Morocha',
+    ubi: 'Poeta Lugones 668',
+    imgPort:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    desc:
+      'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+  },
+]
+export default placesList
---
+++ dapp/data/productsList.js
@@ -0,0 +1,136 @@
+const productsList = [
+  {
+    id: 1,
+    name: 'Entrada comun',
+    place: 'Barto',
+    placeId: 'Barto1',
+    href: '#',
+    price: '48',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageAlt: 'Entrada general',
+  },
+  {
+    id: 2,
+    name: '+ consumicion',
+    place: 'Barto',
+    placeId: 'Barto1',
+    href: '#',
+    price: '48',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageAlt: 'Entrada general mas una consumicion',
+  },
+  {
+    id: 3,
+    name: 'Entrada VIP',
+    place: 'Barto',
+    placeId: 'Barto1',
+    href: '#',
+    price: '48',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageAlt: 'Entrada VIP',
+  },
+  {
+    id: 4,
+    name: 'Entrada VIP 2',
+    place: 'Barto',
+    placeId: 'Barto1',
+    href: '#',
+    price: '48',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageAlt: 'Entrada VIP',
+  },
+  {
+    id: 5,
+    name: 'Entrada comun',
+    place: 'Wallas',
+    placeId: 'Wallas2',
+    href: '#',
+    price: '48',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageAlt: 'Entrada general',
+  },
+  {
+    id: 6,
+    name: '+ consumicion',
+    place: 'Wallas',
+    placeId: 'Wallas2',
+    href: '#',
+    price: '48',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageAlt: 'Entrada general mas una consumicion',
+  },
+  {
+    id: 7,
+    name: 'Entrada VIP',
+    place: 'Wallas',
+    placeId: 'Wallas2',
+    href: '#',
+    price: '48',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageAlt: 'Entrada VIP',
+  },
+  {
+    id: 8,
+    name: 'Entrada VIP 2',
+    place: 'Wallas',
+    placeId: 'Wallas2',
+    href: '#',
+    price: '48',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageAlt: 'Entrada VIP',
+  },
+  {
+    id: 9,
+    name: 'Entrada comun',
+    place: 'Porto',
+    placeId: 'Porto3',
+    href: '#',
+    price: '48',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageAlt: 'Entrada general',
+  },
+  {
+    id: 10,
+    name: '+ consumicion',
+    place: 'Porto',
+    placeId: 'Porto3',
+    href: '#',
+    price: '48',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageAlt: 'Entrada general mas una consumicion',
+  },
+  {
+    id: 11,
+    name: 'Entrada VIP',
+    place: 'Porto',
+    placeId: 'Porto3',
+    href: '#',
+    price: '48',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageAlt: 'Entrada VIP',
+  },
+  {
+    id: 12,
+    name: 'Entrada VIP 2',
+    place: 'Porto',
+    placeId: 'Porto3',
+    href: '#',
+    price: '48',
+    imageSrc:
+      'https://media.istockphoto.com/photos/disco-ball-picture-id1284747487?k=20&m=1284747487&s=612x612&w=0&h=sd4Q25LQRoT9LM4NjTB5yX23kpotq5BdHj73dD8oOU4=',
+    imageAlt: 'Entrada VIP',
+  },
+]
+
+export default productsList
